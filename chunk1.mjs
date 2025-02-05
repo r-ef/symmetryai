ee929a50df64bc6241df438cf4730e24526721de
robert <wiseleet@gmail.com>
2025-02-05 11:45:22 +0400

    Add files via upload

---
+++ chunk1.mjs
@@ -0,0 +1,51 @@
+function t(e, a) {
+  return {
+    description:
+      "Deploy serverless AI agents in the cloud. Orchestrate agent to agent collaboration via the symmetry Python SDK.",
+    favicon:
+      "https://framerusercontent.com/assets/dy3aQIjSwZtFK6jtte6C04C95Nw.png",
+    robots: "max-image-preview:large",
+    socialImage:
+      "https://framerusercontent.com/assets/wHOrnQtx6mKb8zm2DViebGaJyA.jpg",
+    title: "Serverless AI Agents",
+  };
+}
+function i(e, a) {
+  return {
+    bodyClassName: "framer-body-augiA20Il",
+    breakpoints: [
+      { hash: "72rtr7", mediaQuery: "(min-width: 1124px)" },
+      { hash: "5wqan8", mediaQuery: "(max-width: 1123px)" },
+    ],
+    description: t(e, a).description,
+    elements: {
+      c_WLwGrbZ: "faq",
+      DauV_kuQD: "features",
+      f2n0seik6: "homesection",
+      jnnAR5H4i: "bounties",
+      L5gxgwwUc: "documentation",
+      LLbS7xRNZ: "contacts",
+      WqEWNb1yZ: "pricing",
+    },
+    robots: "max-image-preview:large",
+    serializationId: "framer-fEmgo",
+    title: t(e, a).title || "Home",
+    viewport: "width=device-width",
+  };
+}
+var d = 1,
+  p = {
+    exports: {
+      default: {
+        type: "function",
+        annotations: { framerContractVersion: "1" },
+      },
+      metadataVersion: {
+        type: "variable",
+        annotations: { framerContractVersion: "1" },
+      },
+      __FramerMetadata__: { type: "variable" },
+    },
+  };
+export { i as a, d as b, p as c };
+//# sourceMappingURL=chunk-M42P7ECH.mjs.map
